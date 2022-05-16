9c682d34aedb93626560bd573f0a4a0c394e0a46
Miqelad <88131800+Miqelad@users.noreply.github.com>
2022-05-16 14:56:34 +0300

    Create startWindows.bat

---
+++ startWindows.bat
@@ -0,0 +1,5 @@
+"C:\Program Files (x86)\Java\jdk1.8.0_121\bin\java"  
+-Djava.library.path="\\удаленка***" 
+-Dspring.config.location=file:\\расположение файла\config\application.yml 
+-jar "\\удаленка***\application-0.1.jar"
+pause
