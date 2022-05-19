adf0624f21c8938d06e360afbd51029f13d4f805
Miqelad <88131800+Miqelad@users.noreply.github.com>
2022-05-19 16:10:07 +0300

    mail

--- JavaMail.java
+++ JavaMail.java
@@ -38,4 +38,36 @@ public class EmailSenderServices {
         }
 
     }
+
+    public  void  sendEmailNoJSON(String[] toEmail,String subject,String body,String from){
+        /*Для отправки обычного сообщения*/
+/*        SimpleMailMessage message= new SimpleMailMessage();
+        //от
+        message.setFrom(from);//от кого
+        message.setTo(toEmail);//указываем несколько почт
+        message.setText(body);//тело сообщения
+        message.setSubject(subject);//тема
+        //метод отправки самого сообщения
+        mailSender.send(message);//запускает метод
+        //вывод для runtime компилятора, что все отлично
+        */
+        /*Для отправки сообщения в формате html*/
+        try {
+
+            MimeMessage message = mailSender.createMimeMessage();
+
+            message.setSubject(subject);//тема
+            MimeMessageHelper helper;
+            helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);//от кого
+            helper.setTo(toEmail);//указываем несколько почт
+            helper.setText(body, true);//тело сообщения
+
+            mailSender.send(message);
+        } catch (MessagingException ex) {
+            System.err.println("ERROR MAIL SENDING");
+
+        }
+
+    }
 }
