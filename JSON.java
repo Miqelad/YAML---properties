7390b1b53e9fa656da26773737b4d55a5c71d4c2
Miqelad <88131800+Miqelad@users.noreply.github.com>
2022-06-14 12:28:20 +0300

    Update JSON.java

--- JSON.java
+++ JSON.java
@@ -1,5 +1,9 @@
         /*Для удобства в json*/
         List<Structure> list = new ArrayList<>(structureRepository.listStructure());
         File file = new File(files);
-        ObjectMapper mapper=new ObjectMapper(); // специальный класс от jackson для работы с json
+/*Jackson 3.0 меняет ситуацию, поскольку для работы требуется Java 8, и поэтому он может напрямую поддерживать функции.
+Из-за этого parameter-namesи datatypesмодули объединяются jackson-databind и не нуждаются в регистрации;
+datetimeмодуль ( JavaTimeModule) остается отдельным модулем из-за его размера и возможностей настройки.
+Таким образом, вам нужно будет только отдельно добавить модуль "Дата/время Java 8" */
+        ObjectMapper mapper=new ObjectMapper().registerModule(new JavaTimeModule()); // специальный класс от jackson для работы с json
         mapper.writeValue(file,list);
